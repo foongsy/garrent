1f1ff69bbb6d0b99c5909d2d66868297a7e714e8
Henry Fong <foongsy@gmail.com>
2017-03-07 03:54:45 +0800

    writing cli

--- run.py
+++ run.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 import click
 
-@click.command()
-def hello():
-    click.echo('Hi Henry')
+@click.group()
+def run():
+    pass
+
+@run.command()
+def initdb():
+    from garrent import models, database
+    models.Base.metadata.bind = database.engine
+    models.Base.metadata.create_all(database.engine)
+    click.echo("Database inited")
+
+@run.command()
+def updatestock():
+    click.echo('Updating Hong Kong stock list')
+    from garrent.tasks import insert_stock
+    insert_stock()
 
 if __name__ == '__main__':
-    hello()
-
+    run()
 
 """
-from garrent import models, database
-models.Base.metadata.bind = database.engine
-models.Base.metadata.create_all(database.engine)
+
 """
